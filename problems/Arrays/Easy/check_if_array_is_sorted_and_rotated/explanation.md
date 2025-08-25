9c44a687a38872c22dd670b924938d64b1518b9e
nimitsajal <nimitsajal99@gmail.com>
2025-08-25 19:53:57 +0530

    initial setup of directory structures and started template

---
+++ problems/Arrays/Easy/check_if_array_is_sorted_and_rotated/explanation.md
@@ -0,0 +1,29 @@
+# Problem Title
+
+## Problem Statement
+
+
+## Approaches
+
+### Approach 1: 
+- **Intuition:** 
+- **Time:** O()
+- **Space:** O()
+
+### Approach 2: 
+- **Intuition:** 
+- **Time:** O()
+- **Space:** O()
+
+### Approach 3: 
+- **Intuition:** 
+- **Time:** O()
+- **Space:** O()
+
+## Key Points
+- 
+- 
+- 
+
+## Similar Problems
+- 
---
+++ problems/Arrays/Easy/check_if_array_is_sorted_and_rotated/solution.java
@@ -0,0 +1,36 @@
+package problems.Arrays.Easy.check_if_array_is_sorted_and_rotated;
+
+/*
+ * Problem: 
+ * LeetCode: 
+ * Difficulty: 
+ * 
+ * Date: 2025-08-25
+ * Status: 
+ */
+
+class Solution {
+    
+    // Approach 1: 
+    // Time: O(), Space: O()
+    public int approach1(int[] nums) {
+        return 0;
+    }
+    
+    // Approach 2: 
+    // Time: O(), Space: O()
+    public int approach2(int[] nums) {
+        return 0;
+    }
+    
+    // Approach 3: 
+    // Time: O(), Space: O()
+    public int approach3(int[] nums) {
+        return 0;
+    }
+}
+
+
+public class solution {
+    
+}
